7bd598075a15a42edbd2b37083ecb8baadebd898
zhudhjen <zhudhjen@gmail.com>
2014-01-27 11:42:38 +0800

    finishing merge

--- application/config/database.php
+++ application/config/database.php
@@ -49,8 +49,8 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'root';
-$db['default']['password'] = 'oioioi';
+$db['default']['username'] = 'lol_vote';
+$db['default']['password'] = 'lolvote';
 $db['default']['database'] = 'lol';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
