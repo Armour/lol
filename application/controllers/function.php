940c967d943e06ea16626425f521d05bad3c87e1
armour <497052684@qq.com>
2014-01-27 19:46:08 +0800

    Removed useless codes

--- application/controllers/function.php
+++
@@ -1,9 +0,0 @@
-<?php
-	function hashit($c,$a) 
-	{
-		$salt="GC_ZXCVBN".$c;  	
-		$b=$a.$salt;  		
-		$b=md5($b);  		
-		return $b;  			
-	}
-?>
--- application/controllers/index.php
+++ application/controllers/index.php
@@ -1,17 +1,34 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-	class Index extends CI_Controller
+	class Index extends CI_controller
 	{
 		public function __construct()
 		{
-			parent::__construct();
+			parent::__construct();		
+			$this->load->model('vote_model');
+			$this->load->helper('url');
+			$this->load->helper('form');
+		}
+
+		public function index()
+		{	
+			$this->load->view('templates/header');
+			$this->load->view('lol/index');
+			$this->load->view('templates/footer');
 		}
 		
-		public function index()
+		public function vote_voice()
 		{
-			$data = .... ;
 			$this->load->view('templates/header');
-			$this->load->view('vote/index',$data);
+			$this->load->view('lol/voice');
+			$this->load->view('templates/footer');
+		}
+
+		public function vote_skin()
+		{
+			$this->load->view('templates/header');
+			$this->load->view('lol/skin');
 			$this->load->view('templates/footer');
 		}
 	}
+?>
