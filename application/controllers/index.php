f11ab0bcb69c56f6bb31df0b9dde3717aee84273
armour <497052684@qq.com>
2014-01-29 14:52:13 +0800

    rewrite index with parser

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -13,26 +13,19 @@
 
 		public function index()
 		{			
-			$skin1 = $this->vote_model->skin_get_first_skin();
-			$skin2 = $this->vote_model->skin_get_second_skin();
-			$skin3 = $this->vote_model->skin_get_third_skin();
-
-			$voice1 = $this->vote_model->voice_get_first_voice();
-			$voice2 = $this->vote_model->voice_get_second_voice();
-			$voice3 = $this->vote_model->voice_get_third_voice();
-			
-			$this->data['skin1'] = $skin1;
-			$this->data['skin2'] = $skin2;
-			$this->data['skin3'] = $skin3;
-			$this->data['voice1'] = $voice1;
-			$this->data['voice2'] = $voice2;
-			$this->data['voice3'] = $voice3;
-			
-			$data = array(
-				'title' => 'Home'
-				);
-			$this->parser->parse('templates/header', $data);
-			$this->load->view('lol/index',$this->data);
+			$this->data = array(
+				'skin1' => $this->vote_model->skin_get_first_skin(),
+				'skin2' => $this->vote_model->skin_get_second_skin(),
+				'skin3' => $this->vote_model->skin_get_third_skin(),
+				'voice1'=> $this->vote_model->voice_get_first_voice(),	
+				'voice2'=> $this->vote_model->voice_get_second_voice(),	
+				'voice3'=> $this->vote_model->voice_get_third_voice(),	
+			);  
+            $data = array(
+                 'title' => 'Home'
+            );
+            $this->parser->parse('templates/header', $data);
+			$this->parser->parse('lol/index',$this->data);
 			$this->load->view('templates/footer');
 		}
 		
