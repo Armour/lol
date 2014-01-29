986ac708172ae225c9572b6a3215c2e22f05c46a
armour <497052684@qq.com>
2014-01-29 10:57:06 +0800

    add the data which should be get in the index funtion

--- application/controllers/index.php
+++ application/controllers/index.php
@@ -11,9 +11,24 @@
 		}
 
 		public function index()
-		{	
+		{			
+			$skin1 = $this->vote_model->skin_get_first_skin();
+			$skin2 = $this->vote_model->skin_get_second_skin();
+			$skin3 = $this->vote_model->skin_get_third_skin();
+
+			$voice1 = $this->vote_model->voice_get_first_voice();
+			$voice2 = $this->vote_model->voice_get_second_voice();
+			$voice3 = $this->vote_model->voice_get_third_voice();
+			
+			$this->data['skin1'] = $skin1;
+			$this->data['skin2'] = $skin2;
+			$this->data['skin3'] = $skin3;
+			$this->data['voice1'] = $voice1;
+			$this->data['voice2'] = $voice2;
+			$this->data['voice3'] = $voice3;
+			
 			$this->load->view('templates/header');
-			$this->load->view('lol/index');
+			$this->load->view('lol/index',$this->data);
 			$this->load->view('templates/footer');
 		}
 		
