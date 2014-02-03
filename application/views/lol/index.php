9b606231ea5ada78bf2a0558fe1d7a83a4a60e0d
armour <497052684@qq.com>
2014-02-03 17:29:32 +0800

    revise the messy code in index.php (views)

--- application/views/lol/index.php
+++ application/views/lol/index.php
@@ -6,7 +6,7 @@
 					<div class="skin-border"></div>
 					<p class="skin-name">{skin_name}</p>
 					<p class="skin-author">作者：{skin_designer}</p>
-					<p class="skin-votes">当前得票�span class="votes">{skin_vote_number}</span></p>
+					<p class="skin-votes">当前得票：<span class="votes">{skin_vote_number}</span></p>
 					<p class="skin-rank">{skin_rank}</p>
 					<form method="post" accept-charset="utf-8" action="<?php echo site_url("index/vote_skin/{skin_id}") ?>">
 						<button name="vote" class="btn-skin"></button>
@@ -21,7 +21,7 @@
 					<div class="voice-border"></div>
 					<p class="voice-name">{voice_name}</p>
 					<p class="voice-author">作者：{voice_designer}</p>
-					<p class="voice-votes">当前得票�span class="votes">{voice_vote_number}</span></p>
+					<p class="voice-votes">当前得票：<span class="votes">{voice_vote_number}</span></p>
 					<p class="voice-rank">{voice_rank}</p>
 					<form method="post" accept-charset="utf-8" action="<?php echo site_url("index/vote_voice/{voice_id}") ?>">
 						<button name="vote" class="btn-voice"></button>
